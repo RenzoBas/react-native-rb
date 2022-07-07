20d50839280c38d523a03e31653892b1e0a2f4e6
Renzo Basurco <darkgalaxy735@gmail.com>
2022-07-06 17:31:26 -0700

    react commit numbatwo

--- Tabs/Colleges.js
+++ Tabs/Colleges.js
@@ -1,20 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function Colleges() {
     return (
       <View style={styles.container}>
-        <Text> Hello from Colleges</Text>
+        <Text style={styles.collegeHeader}>My Top 3 Colleges I would choose.</Text>
         {/* <Image source={} /> */}
         {/* <StatusBar style="auto" /> */}
-      </View>
+        <FlatList
+        data={[
+          {key: 'University of Southern CA'},
+          {key: 'Pomona College'},
+          {key: 'Thomas Aquinas College'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+        {/* USC, Pomona College, and Thomas Aquinas College */}
+        </View>
     );
   }
   
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: 'darkslategray',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    collegeHeader: {
+      color: "white",
+      fontSize: 40,
+      marginBottom: 50,
+      fontWeight: "bold",
+    },
+
+    item: {
+      color: "cadetblue",
+      fontStyle: "italic",
+      backgroundColor: "lightblue",
+      borderStyle: "solid",
+      borderColor: "darkcyan",
+      fontSize: 25,
+      borderWidth: 25,
+    }
   });
--- Tabs/Profile.js
+++ Tabs/Profile.js
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function Profile() {
   return (
     <View style={styles.container}>
-      <Text> Hello from Profile</Text>
-      {/* <Image source={} /> */}
+      <Text style={styles.header}>Renzo B. -- About Me</Text>
+        <Image source={require('../assets/renzoB.jpeg')} />
       {/* <StatusBar style="auto" /> */}
+      <Text style={styles.para}>Hello, I am Renzo Basurco, I am currently 16 years old, turning 17 soon, and I am learning coding from TXT (Teens Exploring Technology). Some of my hobbies would be: playing video games, listening to music, watching TV, wokring out, playing guitar and now I am learning coding and MMA this summer. What I want to become when I grow up... I want to become a game developer, I believe games are like a form of art, and I as an artist really admire what goes into video games, not only that I also want to be a leader as well, and each day I am learning how to do so. I look and reflect to see what brought me to this point and I am very proud of myself because I made new friends and met a lot of new people in these recent years, I feel more open no than ever before, and I am looking forward to the future!</Text>
     </View>
   );
 }
@@ -13,8 +14,19 @@ export default function Profile() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "darkslategray",
     alignItems: "center",
     justifyContent: "center",
   },
+  header: {
+    backgroundColor: "darkgreen",
+    color: "aquamarine",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  para: {
+    color: "white",
+    backgroundColor: "darkcyan",
+    justifyContent: "center",
+  }
 });
